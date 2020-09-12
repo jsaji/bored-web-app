053d7e6d938f52f10c60f059e9b4aec8554d8205
justinsaji <justinsaji1@gmail.com>
2020-09-12 22:56:48 +1000

    Minor bug fix

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ appserver.py
 - creates an application instance and runs the dev server
 """
 
-from bored_app.app import web_app
+from bored_app.api import web_app
 from bored_app.models import db
 
 if __name__ == '__main__':
